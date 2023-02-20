979a632a0209089063a4e55cd325fa7f176c39e5
Fahmida Sultana Mitu <80353930+FahmidaMitu@users.noreply.github.com>
2023-02-20 20:08:17 +0600

    changes

--- mysite/emp_app/models.py
+++ mysite/emp_app/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Department(models.Model):
     name = models.CharField(max_length=100,null=False)
     location = models.CharField(max_length=100)
 
-def __str__(self):
+    def __str__(self):
         return self.name
- 
+
+
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
 
-def __str__(self):
+    def __str__(self):
         return self.name
-  
+
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100)
@@ -24,9 +28,9 @@ class Employee(models.Model):
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
 
-def __str__(self):
-        return "%s %s %s" %(self.first_name, self.last_name, self.phone)    
 
+    def __str__(self):
+        return "%s %s %s" %(self.first_name, self.last_name, self.phone)
 
 
    
--- mysite/emp_app/urls.py
+++ mysite/emp_app/urls.py
@@ -19,4 +19,9 @@ from.import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
 ]
