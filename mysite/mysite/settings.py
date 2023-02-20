9cd022cfe18456670fc5dcddaae01a733bc55d3d
Fahmida Sultana Mitu <80353930+FahmidaMitu@users.noreply.github.com>
2023-02-20 22:47:56 +0600

    chnges

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8x^#uvk!+$#taaf3!+zitkyw4p(%zrm5my%qhb#w9em0((_snh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
